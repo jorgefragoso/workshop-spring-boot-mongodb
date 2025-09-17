62b716f7804c323870d945cd9fd05ef1de041510
Nelio ALves <alvesjorge06@gmail.com>
2025-09-16 23:20:23 -0300

    PUT criado, CRUD pronto

--- src/main/java/com/jnfragoso/workshopmongo/services/UserService.java
+++ src/main/java/com/jnfragoso/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.jnfragoso.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,13 +31,29 @@ public class UserService {
     public User insert(User obj) {
     	return repo.insert(obj);
     }
+   
     
     public void delete(String id) {
     	findById(id);
     	repo.deleteById(id);
     }
     
-    public User fromDTO(UserDTO objDto) {
+    public User update(User obj) {
+        User newObj = repo.findById(obj.getId())
+                          .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    
+    private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
+	public User fromDTO(UserDTO objDto) {
     return new User(objDto.getId(),objDto.getName(),objDto.getEmail());	
     }
+    
 }
