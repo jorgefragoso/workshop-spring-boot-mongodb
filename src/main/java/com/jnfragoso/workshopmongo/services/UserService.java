b2c854b349c7f8e7735eb370a99839334fd1c412
Nelio ALves <alvesjorge06@gmail.com>
2025-09-16 23:05:39 -0300

    Criando POST

--- src/main/java/com/jnfragoso/workshopmongo/services/UserService.java
+++ src/main/java/com/jnfragoso/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jnfragoso.workshopmongo.domain.User;
+import com.jnfragoso.workshopmongo.dto.UserDTO;
 import com.jnfragoso.workshopmongo.repository.UserRepository;
 import com.jnfragoso.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
     public User findById(String id) {
         return repo.findById(id)
                    .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+    
+    public User insert(User obj) {
+    	return repo.insert(obj);
+    }
+    
+    public User fromDTO(UserDTO objDto) {
+    return new User(objDto.getId(),objDto.getName(),objDto.getEmail());	
     }
 }
